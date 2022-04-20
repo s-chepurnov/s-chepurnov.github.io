956cb01c6b778867388c050fa55b27d2410f06ec
s-chepurnov <Kitaec_31337>
2022-04-20 16:21:31 +0700

    интеграционные тесты на все обработчики

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -2,11 +2,14 @@ package hexlet.code.controllers;
 
 import hexlet.code.model.Url;
 import hexlet.code.model.query.QUrl;
+import io.ebean.PagedList;
 import io.javalin.http.Handler;
 import io.javalin.http.NotFoundResponse;
 
 import java.net.URL;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class UrlController {
 
@@ -27,8 +30,31 @@ public final class UrlController {
 
     private static Handler list = ctx -> {
 
-        List<Url> dbUrls = new QUrl()
-                            .findList();
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
+        final int rowsPerPage = 10;
+
+        PagedList<Url> pagedUrls = new QUrl()
+                .setFirstRow(page * rowsPerPage)
+                .setMaxRows(rowsPerPage)
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+
+        List<Url> dbUrls = pagedUrls.getList();
+
+        int lastPage = pagedUrls.getTotalPageCount() + 1;
+        int currentPage = pagedUrls.getPageIndex() + 1;
+        List<Integer> pages = IntStream
+                .range(1, lastPage)
+                .boxed()
+                .collect(Collectors.toList());
+
+        ctx.attribute("pages", pages);
+        ctx.attribute("currentPage", currentPage);
+
+        //without pagination
+        //List<Url> dbUrls = new QUrl()
+        //.findList();
 
         ctx.attribute("urls", dbUrls);
         ctx.render("url/list.html");
--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,117 @@
 package hexlet.code;
 
+import hexlet.code.model.Url;
+import hexlet.code.model.query.QUrl;
+import io.ebean.DB;
+import io.ebean.Transaction;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AppTest {
 
-    @Test
-    public void test() {
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Transaction transaction;
+    private final int successStatus = 200;
 
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+
+        existingUrl = new Url("https://www.notion.com");
+        existingUrl.save();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    void afterEach() {
+        transaction.rollback();
+    }
+
+    @Nested
+    class RootTest {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest.get(baseUrl).asString();
+            assertThat(response.getStatus()).isEqualTo(successStatus);
+            assertThat(response.getBody()).contains("Анализатор страниц");
+        }
+
+    }
+
+    @Nested
+    class UrlTest {
+
+        @Test
+        void testList() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(successStatus);
+            assertThat(body).contains("Сайты");
+        }
+
+        @Test
+        void testShow() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/" + existingUrl.getId())
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(successStatus);
+            assertThat(body).contains(existingUrl.getName());
+        }
+
+        @Test
+        void testCreate() {
+            String inputName = "https://www.notion.com";
+            HttpResponse<String> responsePost = Unirest
+                    .post(baseUrl + "/urls")
+                    .field("name", inputName)
+                    .asEmpty();
+
+            assertThat(responsePost.getStatus()).isEqualTo(successStatus);
+
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(successStatus);
+            assertThat(body).contains(inputName);
+
+            Url dbUrl = new QUrl()
+                    .name.equalTo(inputName)
+                    .findOne();
+
+            assertThat(dbUrl).isNotNull();
+            assertThat(dbUrl.getName()).isEqualTo(inputName);
+        }
     }
 
 }
